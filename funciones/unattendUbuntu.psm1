1de34e7f38fd7a1bbd8988f9eae465306b785727
barvch <rodolfobaruch97@gmail.com>
2021-02-11 19:51:47 -0800

    unattended ubuntu 16.04

--- funciones/unattendUbuntu.psm1
+++ funciones/unattendUbuntu.psm1
@@ -5,7 +5,6 @@ function Crear-ISOUbuntu {
     $pwhash = bash -c "echo $password | mkpasswd -s -m sha-512"
     $rootPwdHash = bash -c "echo $rootPassword | mkpasswd -s -m sha-512"
     
-    # Creating / Verifying Required Folder Structure is available to work
     Write-Host "Creando carpetas de trabajo para la creacion del ISO..." -ForegroundColor Yellow
     if (-not (Test-Path $tmp)) {
         Write-Host "`tCreando directorio: $tmp" -ForegroundColor Yellow
@@ -40,40 +39,41 @@ function Crear-ISOUbuntu {
     xcopy $ISOSource "$tmp\iso_org\" /e
     
     try {
-        # Copy baseline Kickstart Configuration File To Working Folder
-        Copy-Item ".\recursos\ksUbuntu16.cfg" -Destination "$tmp\iso_org" -Force
-        
-        # Copy baseline Seed File (answers for unattended setup) To Working Folder
-        Copy-Item ".\recursos\semilla.seed" "$tmp\iso_org" -Force
+        Copy-Item ".\recursos\ks.preseed" "$tmp\iso_org" -Force
     } catch {
         ":("
     }
-   
-    
-    # Update the ks.cfg file to reflect encrypted root password hash
-    (Get-Content "$tmp\iso_org\ksUbuntu16.cfg").replace("rootpw --disabled","rootpw --iscrypted $rootPwdHash" ) | Set-Content "$tmp\iso_org\ksUbuntu16.cfg"
-    
+
     # Update the seed file to reflect the users' choices
-    (Get-Content "$tmp\iso_org\semilla.seed").replace('{{username}}', $username) | Set-Content "$tmp\iso_org\$seed_file"
-    (Get-Content "$tmp\iso_org\semilla.seed").replace('{{pwhash}}', $pwhash) | Set-Content "$tmp\iso_org\$seed_file"
-    (Get-Content "$tmp\iso_org\semilla.seed").replace('{{hostname}}', $hostname) | Set-Content "$tmp\iso_org\$seed_file"
-    (Get-Content "$tmp\iso_org\semilla.seed").replace('{{timezone}}', $timezone) | Set-Content "$tmp\iso_org\$seed_file"
-    
-    # Update the isolinux.cfg file to reflect boot time choices
-    #(Get-Content "$tmp\iso_org\isolinux\isolinux.cfg").replace('timeout 0', 'timeout 1') | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
-    #(Get-Content "$tmp\iso_org\isolinux\isolinux.cfg").replace('prompt 0', 'prompt 1') | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
-    
-    # Building installer menu choice to make it default and use ks.cfg and mshende.seed files
-    #$install_lable = "default autoinstall`nlabel autoinstall`nmenu label ^Automatically install Ubuntu`nkernel /install/vmlinuz`nappend file=/cdrom/preseed/ubuntu-server.seed vga=788 initrd=/install/initrd.gz ks=cdrom:/ksUbuntu16.cfg preseed/file=/cdrom/semilla.seed quiet --"
-    $install_lable="default autoinstall`nlabel autoinstall`nmenu label ^Automatically install Ubuntu`nkernel /install/vmlinuz`nappend  ks=/cdrom/semilla.seed auto=true priority=critical debian-installer/locale=en_US keyboard-configuration/layoutcode=us ubiquity/reboot=true languagechooser/language-name=English countrychooser/shortlist=US localechooser/supported-locales=en_US.UTF-8 boot=casper automatic-ubiquity initrd=/install/initrd.gz quiet splash noprompt noshell ---"
-    Clear-Content "$tmp\iso_org\isolinux\isolinux.cfg"
-    #(Get-Content "$tmp\iso_org\isolinux\isolinux.cfg").replace('*', $install_lable) | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
-    $install_lable | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
-    
-    # Creating new ISO file at C:\LabSources\ISOs\ubuntu-16.04.1-server-amd64-unattended.iso
-    Write-Host " Creating the remastered iso"
+    (Get-Content "$tmp\iso_org\ks.preseed").replace('{{username}}', $username) | Set-Content "$tmp\iso_org\$seed_file"
+    (Get-Content "$tmp\iso_org\ks.preseed").replace('{{pwhash}}', $pwhash) | Set-Content "$tmp\iso_org\$seed_file"
+    #(Get-Content "$tmp\iso_org\ks.preseed").replace('{{hostname}}', $hostname) | Set-Content "$tmp\iso_org\$seed_file"
+    #(Get-Content "$tmp\iso_org\ks.preseed").replace('{{timezone}}', $timezone) | Set-Content "$tmp\iso_org\$seed_file"
+
+    # Ubuntu 16
+    $install_lable="default live-install`nlabel live-install`n  menu label ^Install Ubuntu`n  kernel /casper/vmlinuz.efi`n  append  file=/cdrom/ks.preseed auto=true priority=critical debian-installer/locale=en_US keyboard-configuration/layoutcode=us ubiquity/reboot=true languagechooser/language-name=English countrychooser/shortlist=US localechooser/supported-locales=en_US.UTF-8 boot=casper automatic-ubiquity initrd=/casper/initrd.lz quiet splash noprompt noshell ---"
+    #$install_lable="default install`nlabel install`n  menu label ^Automatically install Ubuntu`n  kernel /install/vmlinuz`n  append file=/cdrom/ks.preseed  auto=true priority=critical debian-installer/locale=en_US keyboard-configuration/layoutcode=us ubiquity/reboot=true languagechooser/language-name=English countrychooser/shortlist=US localechooser/supported-locales=en_US.UTF-8 automatic-ubiquity initrd=/install/initrd.gz quiet splash noprompt noshell ---"
+
+    Clear-Content "$tmp\iso_org\isolinux\txt.cfg"
+    $install_lable | Set-Content "$tmp\iso_org\isolinux\txt.cfg"
+    (Get-Content "$tmp\iso_org\isolinux\isolinux.cfg").replace('timeout 0', 'timeout 1') | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
+    (Get-Content "$tmp\iso_org\isolinux\isolinux.cfg").replace('prompt 0', 'prompt 1') | Set-Content "$tmp\iso_org\isolinux\isolinux.cfg"
+    Write-Host "Creando unattended ISO..."
     Set-location "$tmp\iso_org"
-    C:\Users\Administrator\Downloads\mkisofs-md5-2.01-Sample\Binary\Sample\mkisofs.exe -D -r -V "UBUNTU1604SRV" -duplicates-once -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ($tmp+"\"+$hostname+"unattendedISO.iso") .
-   #C:\Users\Administrator\Downloads\mkisofs-md5-2.01-Sample\Binary\Sample\mkisofs.exe -D -r -V "ATTENDLESS_UBUNTU" -duplicates-once -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp+"\"+$hostname+"unattendedISO.iso" .
-    #vmconnect.exe localhost $hostname
+    $rutaIsoSalida = ($tmp+"\"+$hostname+"unattendedISO.iso")
+    C:\Users\Administrator\Downloads\mkisofs-md5-2.01-Sample\Binary\Sample\mkisofs.exe -D -r -V "ubuntu-auto" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $rutaIsoSalida .
+
+    # Create Hyper-v VM
+    $vmFolder = "E:\SanboxTest\tempUbuntu\vhd"
+    New-Item -ItemType Directory -Path $vmFolder -Force | Out-Null
+    $vmSwitch = Get-VMSwitch | Where-Object {$_.Name -eq 'SalidaInternet'}
+    New-VM -Name $hostname -NewVHDPath "$vmFolder\$hostname.vhdx" -NewVHDSizeBytes 15gb -MemoryStartupBytes 1GB
+    Add-VMDvdDrive -VMName $hostname -Path $rutaIsoSalida
+    $vmActiveSwitch = Get-VM -Name $hostname -ErrorAction SilentlyContinue | Get-VMNetworkAdapter -ErrorAction SilentlyContinue
+    if (! $vmActiveSwitch.SwitchName){
+        Write-Host "Adding Switch"
+        Get-VM -Name $hostname | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName $vmSwitch.Name
+    }
+    Start-VM -Name $hostname
+    "Done."
 }
